9e5cdceacf9e3b704e44bdbfca6a54e92ae42a97
Adamity <50904284+adamity@users.noreply.github.com>
2022-10-25 16:57:27 +0800

    update redirect page

--- public/js/redirect.js
+++ public/js/redirect.js
@@ -1,20 +1,13 @@
 const url = new URL(window.location.href).searchParams.get("url");
 const blockedSite = document.getElementById("blockedSite");
 const redirectBtn = document.getElementById('redirectBtn');
-const redirectMemeImg = document.getElementById('redirectMemeImg');
-const redirectMeme = [
-    "https://i.imgflip.com/6riw13.jpg",
-    "https://i.imgflip.com/6rivix.jpg",
-    "https://i.imgflip.com/6riv9c.jpg",
-    "https://i.imgflip.com/6ritiq.jpg",
-    "https://i.imgflip.com/6riwi0.jpg",
-    "https://i.imgflip.com/6riwor.jpg",
-    "https://i.imgflip.com/6riwyc.jpg",
-    "https://i.imgflip.com/6rixnf.jpg"
-];
+
+let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
 blockedSite.innerText = url;
-redirectMemeImg.src = redirectMeme[Math.floor(Math.random() * redirectMeme.length)];
 
 redirectBtn.addEventListener("click", () => {
     window.history.go(-2);
